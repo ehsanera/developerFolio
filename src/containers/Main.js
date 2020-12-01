a24f9800a3bc9060aae39db46333a19c13bc9cb9
ehsan.rafee.gk@gmail.com <ehsan976485>
2020-12-01 18:12:38 +0330

    seo

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
@@ -8,58 +8,56 @@ import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
-import Contact from "./contact/Contact";
 import Footer from "../components/footer/Footer";
 import Talks from "./talks/Talks";
 import Podcast from "./podcast/Podcast";
 import Education from "./education/Education";
 import Top from "./topbutton/Top";
-import Twitter from "./twitter-embed/twitter";
-import { StyleProvider } from "../contexts/StyleContext";
+import {StyleProvider} from "../contexts/StyleContext";
 import "./Main.css";
 import Profile from "./profile/Profile";
-import { educationInfo } from "../portfolio";
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDark: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDark: false,
+        };
+    }
+
+    componentDidMount() {
+        const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+        this.setState({isDark: darkPref.matches});
+    }
+
+    changeTheme = () => {
+        this.setState({isDark: !this.state.isDark});
     };
-  }
 
-  componentDidMount() {
-    const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-    this.setState({ isDark: darkPref.matches });
-  }
-  changeTheme = () => {
-    this.setState({ isDark: !this.state.isDark });
-  };
-
-  render() {
-    return (
-      <div className={this.state.isDark ? "dark-mode" : null}>
-        <StyleProvider
-          value={{ isDark: this.state.isDark, changeTheme: this.changeTheme }}
-        >
-          <Header />
-          <Greeting />
-          <Skills />
-          <StackProgress />
-          <Education />
-          <WorkExperience />
-          <Projects />
-          <StartupProject />
-          <Achievement />
-          <Blogs />
-          <Talks />
-          {/*<Twitter />*/}
-          <Podcast />
-          <Profile />
-          <Footer />
-          <Top />
-        </StyleProvider>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={this.state.isDark ? "dark-mode" : null}>
+                <StyleProvider
+                    value={{isDark: this.state.isDark, changeTheme: this.changeTheme}}
+                >
+                    <Header/>
+                    <Greeting/>
+                    <Skills/>
+                    <StackProgress/>
+                    <Education/>
+                    <WorkExperience/>
+                    <Projects/>
+                    <StartupProject/>
+                    <Achievement/>
+                    <Blogs/>
+                    <Talks/>
+                    {/*<Twitter />*/}
+                    <Podcast/>
+                    <Profile/>
+                    <Footer/>
+                    <Top/>
+                </StyleProvider>
+            </div>
+        );
+    }
 }
